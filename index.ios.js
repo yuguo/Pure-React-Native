50141c44790c687e98386a0e9907ff743ca0b38d
yuguo <fishguo321@gmail.com>
2016-10-29 23:42:34 +0800

    change index to a list

--- index.ios.js
+++ index.ios.js
@@ -8,24 +8,28 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  ListView,
   Text,
   View
 } from 'react-native';
 
 export default class QUI extends Component {
+  constructor(props){
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'QButton', 'QModal'
+      ])
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text style={styles.listTitle}>{rowData}</Text>}
+        />
       </View>
     );
   }
@@ -33,21 +37,13 @@ export default class QUI extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 20,
+    marginLeft: 5,
+    marginRight: 5
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  listTitle: {
+    fontSize: 30
+  }
 });
 
 AppRegistry.registerComponent('QUI', () => QUI);
