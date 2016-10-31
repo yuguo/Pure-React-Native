cbdf7617d5896a81d1d184b6f1f99ea519cb3b36
yuguo <fishguo321@gmail.com>
2016-10-31 14:59:25 +0800

    add small size

--- src/component/buttons/Button.js
+++ src/component/buttons/Button.js
@@ -5,17 +5,12 @@ import Color from 'color';
 
 const styles = StyleSheet.create({
   button: {
-    padding: 19,
-    marginLeft: 15,
-    marginRight: 15,
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'row',
-    borderRadius: 5
+    // flexDirection: 'row'
   },
   text: {
-    color: 'white',
-    fontSize: 18
+    color: 'white'
   },
   disabled: {
     backgroundColor: colors.btDisabled
@@ -31,7 +26,7 @@ export default class Button extends Component {
   }
 
   render(){
-    const {type, onPress, onLongPress, text, bgColor, disabled} = this.props;
+    const {type, onPress, onLongPress, text, bgColor, disabled, size} = this.props;
 
     /* 自定义背景色，或读取type */
     let backgroundColor,
@@ -61,12 +56,44 @@ export default class Button extends Component {
 
     let underlayColor = Color(backgroundColor).darken(0.1).hexString();
 
+    /* 读取size */
+    switch(size){
+      case 'large':
+        sizeStyle = {
+          borderRadius: 5,
+            padding: 19,
+            marginLeft: 15,
+            marginRight: 15,
+        },
+        sizeTextStyle = {
+          fontSize: 18
+        }
+        break;
+      default:
+        sizeStyle = {
+          borderRadius: 2,
+          height: 30,
+          flex: -1,
+          minWidth: 30,
+          paddingLeft: 16,
+          paddingRight: 16,
+          marginLeft: 10,
+          marginRight: 10,
+          marginTop: 24,
+          marginBottom: 24
+        },
+        sizeTextStyle = {
+          fontSize: 14
+        }
+    }
+
     return(
       <TouchableHighlight
         style={[
           styles.button,
           {backgroundColor: backgroundColor},
           borderStyle,
+          sizeStyle,
           disabled && styles.disabled
         ]}
         activeOpacity={1}
@@ -78,6 +105,7 @@ export default class Button extends Component {
           <Text style={[
               styles.text,
               borderTextStyle,
+              sizeTextStyle,
               disabled && styles.disabledText
             ]}
           >
--- src/view/ButtonView.js
+++ src/view/ButtonView.js
@@ -7,48 +7,44 @@ import Button from '../component/buttons/Button';
 export default class ButtonView extends Component {
   render(){
     return(
-      <View style={this.props.style}>
-        <Text>普通按钮</Text>
-        <Button
-          onPress={() => {alert('onPress!')}}
-          onLongPress={() => {alert('onLongPress!')}}
-          text="确认"
-        />
-        <Text>蓝色按钮</Text>
-        <Button
-          type="blue"
-          onPress={() => {alert('onPress!')}}
-          onLongPress={() => {alert('onLongPress!')}}
-          text="确认"
-        />
-        <Text>红色按钮</Text>
-        <Button
-          type="red"
-          onPress={() => {alert('onPress!')}}
-          onLongPress={() => {alert('onLongPress!')}}
-          text="警告"
-        />
-        <Text>Disable按钮</Text>
-        <Button
-          onPress={() => {alert('onPress!')}}
-          onLongPress={() => {alert('onLongPress!')}}
-          disabled={true}
-          text="Disabled"
-        />
-        <Text>自定义按钮</Text>
-        <Button
-          onPress={() => {alert('onPress!')}}
-          onLongPress={() => {alert('onLongPress!')}}
-          bgColor="blue"
-          text="自定义按钮"
-        />
-        <Text>自定义按钮Disabled（统一灰色）</Text>
-        <Button
-          onPress={() => {alert('onPress!')}}
-          onLongPress={() => {alert('onLongPress!')}}
-          disabled={true}
-          text="Disabled"
-        />
+      <View>
+        <View style={[this.props.style, {
+            flexWrap: 'wrap',
+            alignItems: 'flex-start',
+            flexDirection:'row'
+          }]}>
+          <Button
+            onPress={() => {alert('onPress!')}}
+            onLongPress={() => {alert('onLongPress!')}}
+            text="按钮"
+          />
+          <Button
+            type="blue"
+            onPress={() => {alert('onPress!')}}
+            onLongPress={() => {alert('onLongPress!')}}
+            text="确认"
+          />
+          <Button
+            type="red"
+            onPress={() => {alert('onPress!')}}
+            onLongPress={() => {alert('onLongPress!')}}
+            text="警告"
+          />
+          <Button
+            onPress={() => {alert('onPress!')}}
+            onLongPress={() => {alert('onLongPress!')}}
+            disabled={true}
+            text="Disabled"
+          />
+          <Button
+            onPress={() => {alert('onPress!')}}
+            onLongPress={() => {alert('onLongPress!')}}
+            bgColor="blue"
+            text="自定义按钮"
+          />
+        </View>
+
+        
       </View>
     )
   }
