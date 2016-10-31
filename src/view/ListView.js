658dcde7849806813acbca58c485d3163d471d5e
yuguo <fishguo321@gmail.com>
2016-10-31 16:09:12 +0800

    add list and other components

---
+++ src/view/ListView.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+});
+
+export default class ListView extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <Text>List</Text>
+    )
+  }
+}
