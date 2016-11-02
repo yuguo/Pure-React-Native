7df237826255e223313baf164c76aa76cf5fa2bb
yuguo <fishguo321@gmail.com>
2016-11-02 16:35:10 +0800

    add animation

--- src/component/tips/Toast.js
+++ src/component/tips/Toast.js
@@ -1,31 +1,56 @@
 import React, {Component} from 'react';
-import {Modal, View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import {Modal, View, Text, TouchableOpacity, StyleSheet, Image, Animated} from 'react-native';
 
 export default class Toast extends Component {
   constructor(props){
     super(props);
-    this.state = {modalVisible: true};
+    this.state = {
+      modalVisible: true,
+      modalTop: new Animated.Value(-50),
+    };
   }
 
   _setModalVisible(visible){
-    this.setState({modalVisible: visible});
+    this.setState({
+      modalVisible: visible
+    });
+  }
+
+  componentDidMount(){
+    this.state.modalTop.setValue(-50);
+    Animated.spring(
+      this.state.modalTop,
+      {
+        toValue: 0,
+        friction: 7
+      }
+    ).start();
+  }
+
+  componentWillUnmount(){
+    Animated.spring(
+      this.state.modalTop,
+      {
+        toValue: -50,
+        friction: 7
+      }
+    ).start();
   }
 
   render(){
     const { backgroundColor } = this.props;
     return(
       <Modal
-        animationType={"slide"}
-        style={{backgroundColor:'blue'}}
         visible={this.state.modalVisible}
+        onRequestClose={() => {alert("Modal has been closed.")}}
       >
-        <View style={[styles.toast, {backgroundColor: backgroundColor}]}>
+        <Animated.View style={[styles.toast, {top: this.state.modalTop, backgroundColor: backgroundColor}]}>
           <TouchableOpacity onPress={() => {
-              this._setModalVisible(!this.state.modalVisible)
+              this._setModalVisible(!this.state.modalVisible);
             }}>
             <Text style={styles.text}>Hide Modal</Text>
           </TouchableOpacity>
-        </View>
+        </Animated.View>
       </Modal>
     )
   }
@@ -33,13 +58,11 @@ export default class Toast extends Component {
 
 const styles = StyleSheet.create({
   toast: {
-    // position: 'absolute',
-    height:50,
-    top:0,
-    left:0,
-    flexDirection: 'row',
+    paddingTop: 20,
+    height: 50,
+    position: 'relative',
     zIndex: 10000,
-    alignItems: 'center'
+    alignItems: 'flex-start'
   },
   text: {
     color: 'white',
